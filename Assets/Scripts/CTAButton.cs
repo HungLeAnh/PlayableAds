b090ab3d9ba313a3e76029ca54f6e2da9a69344e
Lê Anh Hùng <leanhung.bhn@gmail.com>
2026-04-22 16:56:14 +0700

    update CTA button

--- Assets/Scripts/CTAButton.cs
+++ Assets/Scripts/CTAButton.cs
@@ -1,8 +1,42 @@
 using UnityEngine;
 using Luna.Unity;
+using System.Collections;
 
 public class CTAButton : MonoBehaviour
 {
+    public float pulseSpeed = 4f;
+    public float pulseAmount = 0.1f;
+    
+    private Vector3 originalScale;
+    private Coroutine pulseCoroutine;
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    void OnEnable()
+    {
+        if (pulseCoroutine != null) StopCoroutine(pulseCoroutine);
+        pulseCoroutine = StartCoroutine(Pulse());
+    }
+
+    void OnDisable()
+    {
+        if (pulseCoroutine != null) StopCoroutine(pulseCoroutine);
+        transform.localScale = originalScale;
+    }
+
+    IEnumerator Pulse()
+    {
+        while (true)
+        {
+            float scale = 1f + Mathf.Sin(Time.time * pulseSpeed) * pulseAmount;
+            transform.localScale = originalScale * scale;
+            yield return null;
+        }
+    }
+
     public void OpenStore()
     {
         LifeCycle.GameEnded();
